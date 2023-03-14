377634f6720760b919707e4515ec4f1f44b7de2e
[kellecivolkan@gmail.com] <kellecivolkan@gmail.com>
2023-03-14 21:21:08 +0300

    Glide Created

--- app/src/main/java/com/example/clonesocialapp/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/clonesocialapp/fragments/ProfileFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.clonesocialapp.R
+import com.example.clonesocialapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    private var _binding:FragmentProfileBinding?=null
+    private val binding get()=_binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +19,27 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?,
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding= FragmentProfileBinding.inflate(inflater,container,false)
+        val view=binding.root
+        return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.followButton.setOnClickListener{
+            showUnfollowButton(view)
+        }
+
+
+    }
+    private fun showUnfollowButton(view:View){
+
+    }
+
 
 
 }
--- app/src/main/java/com/example/clonesocialapp/home/HomeFragment.kt
+++ app/src/main/java/com/example/clonesocialapp/home/HomeFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.clonesocialapp.R
 
 class HomeFragment : Fragment() {
+    private lateinit var adapter: HomeAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,4 +25,25 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+         val swipeCallback=object :ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder,
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val layoutPosition=viewHolder.layoutPosition
+                val selectedArt=adapter.takeAllPicture[layoutPosition]
+                //viewModel.deleteArt(selectedArt)
+            }
+    }
+        
+
+}
 }
