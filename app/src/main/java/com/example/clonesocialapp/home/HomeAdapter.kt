f6a368f1c756143d099effe8ffcca35cc31de9de
[kellecivolkan@gmail.com] <kellecivolkan@gmail.com>
2023-03-14 16:06:28 +0300

    Glide Created

--- app/src/main/java/com/example/clonesocialapp/home/HomeAdapter.kt
+++ app/src/main/java/com/example/clonesocialapp/home/HomeAdapter.kt
@@ -1,20 +1,24 @@
 package com.example.clonesocialapp.home
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.clonesocialapp.R
 
-class HomeAdapter: RecyclerView.Adapter<HomeAdapter.HomeFragmentVH>() {
+class HomeAdapter(val takeAllPicture:ArrayList<PicturesData>): RecyclerView.Adapter<HomeAdapter.HomeFragmentVH>() {
     class HomeFragmentVH(itemView:View):RecyclerView.ViewHolder(itemView) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeFragmentVH {
-
+        val inflater= LayoutInflater.from(parent.context)
+        val viewHolder=inflater.inflate(R.layout.pictures_raw,parent,false)
+        return HomeFragmentVH(viewHolder)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return takeAllPicture.size
     }
 
     override fun onBindViewHolder(holder: HomeFragmentVH, position: Int) {
