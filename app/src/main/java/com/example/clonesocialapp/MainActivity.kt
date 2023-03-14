4a57685d53af231eef39554d3125824060c290e1
[kellecivolkan@gmail.com] <kellecivolkan@gmail.com>
2023-03-14 12:26:10 +0300

    UI Side training

--- app/src/main/java/com/example/clonesocialapp/MainActivity.kt
+++ app/src/main/java/com/example/clonesocialapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.clonesocialapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    private fun loadFragment(){
+
     }
 }
