21cdfbab91b35afce2b36d5fad812db2f5acf8ca
[kellecivolkan@gmail.com] <kellecivolkan@gmail.com>
2023-03-15 18:23:03 +0300

    Search Fragment RView Done

--- app/src/main/java/com/example/clonesocialapp/home/HomeAdapter.kt
+++ app/src/main/java/com/example/clonesocialapp/home/HomeAdapter.kt
@@ -3,17 +3,20 @@ package com.example.clonesocialapp.home
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.clonesocialapp.R
+import com.squareup.picasso.Picasso
 
-class HomeAdapter(val takeAllPicture:ArrayList<PicturesData>): RecyclerView.Adapter<HomeAdapter.HomeFragmentVH>() {
+class HomeAdapter(val takeAllPicture:List<PicturesData>): RecyclerView.Adapter<HomeAdapter.HomeFragmentVH>() {
     class HomeFragmentVH(itemView:View):RecyclerView.ViewHolder(itemView) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeFragmentVH {
         val inflater= LayoutInflater.from(parent.context)
-        val viewHolder=inflater.inflate(R.layout.pictures_raw,parent,false)
+        val viewHolder=inflater.inflate(R.layout.images_raw,parent,false)
         return HomeFragmentVH(viewHolder)
     }
 
@@ -22,6 +25,9 @@ class HomeAdapter(val takeAllPicture:ArrayList<PicturesData>): RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: HomeFragmentVH, position: Int) {
-        TODO("Not yet implemented")
+
+        val imageView=holder.itemView.findViewById<ImageView>(R.id.imageView)
+        val holderPictures=takeAllPicture[position]
+        Picasso.get().load(holderPictures.pictures).into(imageView);
     }
 }
--- app/src/main/java/com/example/clonesocialapp/home/HomeFragment.kt
+++ app/src/main/java/com/example/clonesocialapp/home/HomeFragment.kt
@@ -1,17 +1,24 @@
 package com.example.clonesocialapp.home
 
+import android.media.Image
 import android.os.Bundle
+import android.provider.MediaStore.Images
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.clonesocialapp.R
+import com.example.clonesocialapp.databinding.FragmentHomeBinding
+import com.example.clonesocialapp.databinding.FragmentProfileBinding
 
 class HomeFragment : Fragment() {
     private lateinit var adapter: HomeAdapter
-
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,15 +26,41 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?,
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val images= listOf<PicturesData>(
+            PicturesData(R.drawable.other),
+            PicturesData(R.drawable.man),
+            PicturesData(R.drawable.another),
+            PicturesData(R.drawable.post),
+            PicturesData(R.drawable.other),
+            PicturesData(R.drawable.man),
+            PicturesData(R.drawable.another),
+            PicturesData(R.drawable.post),
+            PicturesData(R.drawable.other),
+            PicturesData(R.drawable.man),
+            PicturesData(R.drawable.another),
+            PicturesData(R.drawable.post),
 
+        )
+        val layoutManager = LinearLayoutManager(activity)
+        binding.homeRV.layoutManager = layoutManager
+        adapter = HomeAdapter(images)
+        binding.homeRV.adapter=adapter
+        //span count kaç tane görsel gösterileceği !!
+        binding.homeRV.layoutManager= GridLayoutManager(requireContext(),3)
+
+
+
+        /* swipe callback
          val swipeCallback=object :ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
             override fun onMove(
                 recyclerView: RecyclerView,
@@ -42,8 +75,8 @@ class HomeFragment : Fragment() {
                 val selectedArt=adapter.takeAllPicture[layoutPosition]
                 //viewModel.deleteArt(selectedArt)
             }
-    }
-        
 
-}
+          */
+    }
+
 }
